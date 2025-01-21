709098c5f94a8e4f9dc5f5d6683ae97c7a1de6f0
Caroline Ott <caroline.ott1994@gmail.com>
2025-01-21 13:19:44 +0100

    change requirements for computationalWorkflow

--- profile/scripts/computationalWorkflowCreation.fsx
+++ profile/scripts/computationalWorkflowCreation.fsx
@@ -20,118 +20,145 @@ let requiredProfileProperties = [
     ProfileRow.create("dct:conformsTo",     Required, SPECIFIC 2,   [   (IRI, END)], 
                                                                     "Used to state the profiles that the markup relates to. MUST be 'https://bioschemas.org/profiles/ComputationalWorkflow/1.0-RELEASE' AND `<insert our LabProtocol profile IRI here>`", 
                                                                     "https://bioschemas.org/profiles/ComputationalWorkflow")
-    ProfileRow.create("creator",            Required, MANY,         [   (Schema.Organization, OR)
-                                                                        (Schema.Person, END)
-                                                                    ], 
-                                                                    "The creator/author of this CreativeWork. This is the same as the Author property for CreativeWork.", 
-                                                                    "https://bioschemas.org/profiles/ComputationalWorkflow")
-    ProfileRow.create("dateCreated",        Required, ONE,          [   (Schema.Date, OR)   
-                                                                        (Schema.DateTime, END)
-                                                                    ], 
-                                                                    "The date on which the CreativeWork was created or the item was added to a DataFeed.", 
-                                                                    "https://bioschemas.org/profiles/ComputationalWorkflow")
     ProfileRow.create("input",              Required, MANY,         [   (BioSchemas.FormalParameter, END)], 
                                                                     "An input required to use the computational workflow (eg. Excel spreadsheet, BAM file)", 
                                                                     "https://bioschemas.org/profiles/ComputationalWorkflow")
-    ProfileRow.create("license",            Required, MANY,         [   (Schema.CreativeWork, OR)
-                                                                        (Schema.URL, END)
-                                                                    ], 
-                                                                    "A license document that applies to this content, typically indicated by URL.", 
-                                                                    "https://bioschemas.org/profiles/ComputationalWorkflow")
-    ProfileRow.create("name",               Required, ONE,          [   (Schema.Text, END)], 
-                                                                    "The name of the item.", 
-                                                                    "https://bioschemas.org/profiles/ComputationalWorkflow")
     ProfileRow.create("output",             Required, MANY,         [   (BioSchemas.FormalParameter, END)], 
                                                                     "An output produced by the workflow", 
-                                                                    "https://bioschemas.org/profiles/ComputationalWorkflow")
-    ProfileRow.create("programmingLanguage",Required, MANY,         [   (Schema.ComputerLanguage, OR)
-                                                                        (Schema.Text, END)
-                                                                    ], 
-                                                                    "The computer programming language, Scripts written in a programming language, as well as workflows, generally need a runtime; in RO-Crate the runtime SHOULD be indicated using a liberal interpretation of programmingLanguage", 
-                                                                    "https://bioschemas.org/profiles/ComputationalWorkflow")
-    ProfileRow.create("sdPublisher",        Required, ONE,          [   (Schema.Organization, OR)
-                                                                        (Schema.Person, END)
-                                                                    ], 
-                                                                    "The host site for the ComputationalWorkflow", 
-                                                                    "https://bioschemas.org/profiles/ComputationalWorkflow")
-    ProfileRow.create("url",                Required, ONE,          [   (Schema.URL, END)], 
-                                                                    "URL of the item.", 
-                                                                    "https://bioschemas.org/profiles/ComputationalWorkflow")
-    ProfileRow.create("version",            Required, ONE,          [   (Schema.Text, OR)
-                                                                        (Schema.Number, END)
-                                                                    ], 
-                                                                    "Version is a release. The date modified may not warrant a release, but last date modified and access to all versions is important", 
                                                                     "https://bioschemas.org/profiles/ComputationalWorkflow")
     // properties from https://bioschemas.org/profiles/ComputationalWorkflow
 ]
 
 let recommendedProfileRows = [
+    // Required -> Recommended
+    ProfileRow.create("creator",            Required, MANY,         [   (Schema.Organization, OR)
+                                                                        (Schema.Person, END)
+                                                                    ], 
+                                                                    "The creator/author of this CreativeWork. This is the same as the Author property for CreativeWork.", 
+                                                                    "https://bioschemas.org/profiles/ComputationalWorkflow")
+    // Required -> Recommended
+    ProfileRow.create("dateCreated",        Required, ONE,          [   (Schema.Date, OR)   
+                                                                        (Schema.DateTime, END)
+                                                                    ], 
+                                                                    "The date on which the CreativeWork was created or the item was added to a DataFeed.", 
+                                                                    "https://bioschemas.org/profiles/ComputationalWorkflow")
+    // Required -> Recommended
+    ProfileRow.create("license",            Required, MANY,         [   (Schema.CreativeWork, OR)
+                                                                        (Schema.URL, END)
+                                                                    ], 
+                                                                    "A license document that applies to this content, typically indicated by URL.", 
+                                                                    "https://bioschemas.org/profiles/ComputationalWorkflow")
+    // Required -> Recommended
+    ProfileRow.create("name",               Required, ONE,          [   (Schema.Text, END)], 
+                                                                    "The name of the item.", 
+                                                                    "https://bioschemas.org/profiles/ComputationalWorkflow")
+    // Required -> Recommended
+    ProfileRow.create("programmingLanguage",Required, MANY,         [   (Schema.ComputerLanguage, OR)
+                                                                        (Schema.Text, END)
+                                                                    ], 
+                                                                    "The computer programming language, Scripts written in a programming language, as well as workflows, generally need a runtime; in RO-Crate the runtime SHOULD be indicated using a liberal interpretation of programmingLanguage", 
+                                                                    "https://bioschemas.org/profiles/ComputationalWorkflow")
+    // Required -> Recommended
+    ProfileRow.create("sdPublisher",        Required, ONE,          [   (Schema.Organization, OR)
+                                                                        (Schema.Person, END)
+                                                                    ], 
+                                                                    "The host site for the ComputationalWorkflow", 
+                                                                    "https://bioschemas.org/profiles/ComputationalWorkflow")
+    // Required -> Recommended
+    ProfileRow.create("url",                Required, ONE,          [   (Schema.URL, END)], 
+                                                                    "URL of the item.", 
+                                                                    "https://bioschemas.org/profiles/ComputationalWorkflow")
+    // Required -> Recommended
+    ProfileRow.create("version",            Required, ONE,          [   (Schema.Text, OR)
+                                                                        (Schema.Number, END)
+                                                                    ], 
+                                                                    "Version is a release. The date modified may not warrant a release, but last date modified and access to all versions is important", 
+                                                                    "https://bioschemas.org/profiles/ComputationalWorkflow")
+]
+
+let optionalProfileRows = [
+
     // properties from https://bioschemas.org/profiles/ComputationalWorkflow
+    // Recommended -> Optional
     ProfileRow.create("citation",               Recommended, ANY,   [   (Schema.CreativeWork, OR)
                                                                         (Schema.Text, END)
                                                                     ], 
                                                                     "A citation or reference to another creative work, such as another publication, web page, scholarly article, etc.", 
                                                                     "https://bioschemas.org/profiles/ComputationalWorkflow")
+    // Recommended -> Optional
     ProfileRow.create("contributor",            Recommended, ANY,   [   (Schema.Organization, OR)
                                                                         (Schema.Person, END)
                                                                     ], 
                                                                     "A secondary contributor to the CreativeWork or Event.", 
                                                                     "https://bioschemas.org/profiles/ComputationalWorkflow")
+    // Recommended -> Optional
     ProfileRow.create("creativeWorkStatus",     Recommended, ONE,   [   (Schema.DefinedTerm, OR)
                                                                         (Schema.Text, END)
                                                                     ], 
                                                                     "The status of a creative work in terms of its stage in a lifecycle. Example terms include Incomplete, Draft, Published, Obsolete. Some organizations define a set of terms for the stages of their publication lifecycle.", 
                                                                     "https://bioschemas.org/profiles/ComputationalWorkflow")
+    // Recommended -> Optional
     ProfileRow.create("description",            Recommended, ONE,   [   (Schema.Text, END)], 
                                                                     "A description of the item.", 
                                                                     "https://bioschemas.org/profiles/ComputationalWorkflow")
+    // Recommended -> Optional
     ProfileRow.create("documentation",          Recommended, ANY,   [   (Schema.CreativeWork, OR)
                                                                         (Schema.URL, END)
                                                                     ], 
                                                                     "Documentation describing the ComputationalWorkflow and its use.", 
                                                                     "https://bioschemas.org/profiles/ComputationalWorkflow")
+    // Recommended -> Optional
     ProfileRow.create("funding",                Recommended, ANY,   [   (Schema.Grant, END)], 
                                                                     "The funding for the workflow", 
                                                                     "https://bioschemas.org/profiles/ComputationalWorkflow")
+    // Recommended -> Optional
     ProfileRow.create("hasPart",                Recommended, ANY,   [   (Schema.CreativeWork, END)], 
                                                                     "The tools/scripts that are (potentially) used by the ComputationalWorkflow when it is executed, The parts are not ordered; they normally correspond to steps in the workflow, there is no specified mapping.", 
                                                                     "https://bioschemas.org/profiles/ComputationalWorkflow")
+    // Recommended -> Optional
     ProfileRow.create("isBasedOn",              Recommended, ONE,   [   (Schema.CreativeWork, OR)
                                                                         (Schema.Product, END)
                                                                         (Schema.URL, END)
                                                                     ], 
                                                                     "This is normally another ComputationalWorkflow, but may also be, for example, a paper or a lab protocol.", 
                                                                     "https://bioschemas.org/profiles/ComputationalWorkflow")
+    // Recommended -> Optional
     ProfileRow.create("keywords",               Recommended, ONE,   [   (Schema.Text, END)], 
                                                                     "Keywords or tags used to describe this content. Multiple entries in a keywords list are typically delimited by commas.", 
                                                                     "https://bioschemas.org/profiles/ComputationalWorkflow")
+    // Recommended -> Optional
     ProfileRow.create("maintainer",             Recommended, ANY,   [   (Schema.Organization, OR)
                                                                         (Schema.Person, END)
                                                                     ], 
                                                                     "A maintainer of a Dataset, software package (SoftwareApplication), or other Project. A maintainer is a Person or Organization that manages contributions to, and/or publication of, some (typically complex) artifact. It is common for distributions of software and data to be based on “upstream” sources. When maintainer is applied to a specific version of something e.g. a particular version or packaging of a Dataset, it is always possible that the upstream source has a different maintainer. The isBasedOn property can be used to indicate such relationships between datasets to make the different maintenance roles clear. Similarly in the case of software, a package may have dedicated maintainers working on integration into software distributions such as Ubuntu, as well as upstream maintainers of the underlying work.", 
                                                                     "https://bioschemas.org/profiles/ComputationalWorkflow")
+    // Recommended -> Optional
     ProfileRow.create("producer",               Recommended, ANY,   [   (Schema.Organization, OR)
                                                                         (Schema.Person, END)
                                                                     ], 
                                                                     "The person or organization who produced the workflow", 
                                                                     "https://bioschemas.org/profiles/ComputationalWorkflow")
+    // Recommended -> Optional
     ProfileRow.create("publisher",              Recommended, ANY,   [   (Schema.Organization, OR)
                                                                         (Schema.Person, END)
                                                                     ], 
                                                                     "Where it came came from, e.g. Galaxy, github, or WF Hub if uploaded", 
                                                                     "https://bioschemas.org/profiles/ComputationalWorkflow")
+    // Recommended -> Optional
     ProfileRow.create("runtimePlatform",        Recommended, ANY,   [   (Schema.Text, END)], 
                                                                     "Runtime platform or script interpreter dependencies (Example - Java v1, Python2.3, .Net Framework 3.0). Supersedes runtime.", 
                                                                     "https://bioschemas.org/profiles/ComputationalWorkflow")
+    // Recommended -> Optional
     ProfileRow.create("softwareRequirements",   Recommended, ANY,   [   (Schema.Text, END)], 
                                                                     "Renaming schema.org/requirements as softwareRequirements", 
                                                                     "https://bioschemas.org/profiles/ComputationalWorkflow")
+    // Recommended -> Optional
     ProfileRow.create("targetProduct",          Recommended, ANY,   [   (Schema.SoftwareApplication, END)], 
                                                                     "Target Operating System / Product to which the code applies. If applies to several versions, just the product name can be used.", 
                                                                     "https://bioschemas.org/profiles/ComputationalWorkflow")
-
     // properties from OUR LabProtocol profile
     // description is already part of ComputationalWorkflow profile
+    // Recommended -> Optional
     ProfileRow.create("intendedUse",            Recommended, ONE,   [   (Schema.DefinedTerm, OR)
                                                                         (Schema.Text, OR)
                                                                         (Schema.URL, END)
@@ -139,10 +166,6 @@ let recommendedProfileRows = [
                                                                     "The protocol type as an ontology term", 
                                                                     "isa-ro-crate-profile/LabProtocol")
     // name is already part of ComputationalWorkflow profile
-]
-
-let optionalProfileRows = [
-
     // properties from https://bioschemas.org/profiles/ComputationalWorkflow
     ProfileRow.create("alternateName",          Optional, ANY,      [   (Schema.Text, END)], 
                                                                     "An alias for the item.", 
